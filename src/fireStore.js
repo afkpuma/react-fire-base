57a26abd5c799d255b8d387c716339c4199f538a
afkpuma <afk.puma@gmail.com>
2022-10-10 14:57:42 -0300

    Conexao FireStore

--- src/fireStore.js
+++ src/fireStore.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const ao = initializeApp(firebaseConfig);
+const db = getFirestore(ap);
 
 export default db

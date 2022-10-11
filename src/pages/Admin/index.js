8e9dfc007aff872060307a8f450e24811da4a200
afkpuma <afk.puma@gmail.com>
2022-10-10 23:52:35 -0300

    .

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,9 +1,44 @@
+import {useState} from 'react'
+import auth from '../../fireAuth'
+import {signOut} from 'firebase/auth'
 import './admin.css'
 
 export default function Admin(){
+  const [tarefaInput, setTarefaInput] = useState('')
+
+  function handleRegister(e){
+    e.preventDefault()
+    alert('clicou')
+  }
+
+  async function handleLogout(){
+    await signOut(auth)
+  }
+
+
   return(
-    <div>
-      <h1> Pagina ADMIN!</h1>
+    <div className='admin-container'>
+      <h1>Minhas tarefas</h1>
+
+      <form className = 'form' onSubmit={handleRegister}>
+        <textarea 
+        placeholder= 'digite sua tarefa'
+        value={tarefaInput}
+        onChange = {(e)=>setTarefaInput(e.target.value)}
+        />
+        <button className='btn-register' type='submit'>Registrar Tarefa</button>
+      </form>
+
+      <article className='list'>
+        <p>JS e React</p>
+
+        <div>
+          <button>Editar</button>
+          <button className='btn-delete'>Concluir</button>
+        </div>
+      </article>
+
+      <button className='btn-logout' onClick={handleLogout}>Sair</button>
     </div>
   )
 }
